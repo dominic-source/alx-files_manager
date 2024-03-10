2a504223c12434309b24a0c1ef57a0d8ce40af45
dominic-source <chinonsodomnic@gmail.com>
2024-03-10 15:08:19 -0700

    finding bug

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
-
+console.log(UsersController.postNew, AppController.getStats);
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
