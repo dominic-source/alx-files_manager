b41aea7f8ea0e5b28f74957eed05801683d2c29c
dominic-source <chinonsodomnic@gmail.com>
2024-03-11 10:29:57 -0700

    update server file

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import router from './routes/index';
 
 // Initialize express app
 const app = express();
 
 // Use JSON body parser middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Use URL-encoded body parser middleware
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', router);
-app.listen(process.env.PORT || 5000, () => {
-  console.log('Server is listening on port 5000 or PORT env!');
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
 
-module.exports = app;
+export default app;
