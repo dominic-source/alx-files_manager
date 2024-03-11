0fe137844589bb113dba2ad06dde4503d6be4d8f
dominic-source <chinonsodomnic@gmail.com>
2024-03-11 02:57:09 -0700

    debug code

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,25 +4,27 @@ import dbClient from '../utils/db';
 async function postNew(req, res) {
   const { email } = req.body;
   if (!email) {
-    res.status(400).json({ error: 'Missing email' });
+    return res.status(400).json({ error: 'Missing email' });
   }
   const { password } = req.body;
   if (!password) {
-    res.status(400).json({ error: 'Missing password' });
+    return res.status(400).json({ error: 'Missing password' });
   }
   const collection = dbClient.db.collection('users');
+  const hashPassword = sha1(password);
+  const result = await collection.insertOne({ email, password: hashPassword });
   collection.findOne({ email })
     .then((user) => {
       if (user) {
-        res.status(400).json({ error: 'Already exist' });
+        return res.status(400).json({ error: 'Already exist' });
       }
+      return res.status(201).json({ email, id: result.insertedId });
     })
     .catch((err) => {
       console.log(err);
+      return res.status(500).json({ error: 'Internal server error' });
     });
-  const hashPassword = sha1(password);
-  const result = await collection.insertOne({ email, password: hashPassword });
-  res.status(201).json({ email, id: result.insertedId });
+  return res.status(201).json({ email, id: result.insertedId });
 }
 
 module.exports = { postNew };
